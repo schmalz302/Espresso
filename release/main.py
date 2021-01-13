91b59c6f8e90aa11d335d070a4945182f8be6362
buk55 <boltneffe@yandex.ru>
2021-01-13 17:20:56 +0500

    Латте макиато

--- release/main.py
+++ release/main.py
@@ -1,16 +1,15 @@
 import sqlite3
 import sys
-
-from PyQt5 import uic
+from release.addEditCoffeeForm import Ui_MainWindow
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtWidgets import QMainWindow, QTableWidgetItem
 
 
-class MyWidget(QMainWindow):
+class MyWidget(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi("addEditCoffeeForm.ui", self)
-        self.con = sqlite3.connect("coffee.sqlite.db")
+        self.setupUi(self)
+        self.con = sqlite3.connect("release/data/coffee.sqlite.db")
         self.pushButton.clicked.connect(self.save_results)
         self.tableWidget.itemChanged.connect(self.item_changed)
         self.pushButton_2.clicked.connect(self.update_result)
