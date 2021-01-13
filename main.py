e3f75b9ef8d92bfbf2ef34d5767e8ba3db843531
buk55 <boltneffe@yandex.ru>
2021-01-13 17:03:33 +0500

    Капучино

--- main.py
+++ main.py
@@ -6,36 +6,60 @@ from PyQt5.QtWidgets import QApplication
 from PyQt5.QtWidgets import QMainWindow, QTableWidgetItem
 
 
-class DBSample(QMainWindow):
+class MyWidget(QMainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('main.ui', self)
-        self.connection = sqlite3.connect("coffee.sqlite.db")
-        self.pushButton.clicked.connect(self.select_data)
-        self.textEdit.setPlainText("SELECT * FROM coffee")
-        self.select_data()
+        uic.loadUi("addEditCoffeeForm.ui", self)
+        self.con = sqlite3.connect("coffee.sqlite.db")
+        self.pushButton.clicked.connect(self.save_results)
+        self.tableWidget.itemChanged.connect(self.item_changed)
+        self.pushButton_2.clicked.connect(self.update_result)
+        self.modified = {}
+        self.titles = None
 
-    def select_data(self):
+    def update_result(self):
+        cur = self.con.cursor()
+        # Получили результат запроса, который ввели в текстовое поле
         query = self.textEdit.toPlainText()
-        res = self.connection.cursor().execute(query).fetchall()
-        # Заполним размеры таблицы
-        self.tableWidget.setColumnCount(len(res[0]))
-        self.tableWidget.setRowCount(len(res))
-        print(len(res))
-        # Заполняем таблицу элементами
-        for i, row in enumerate(res):
-            self.tableWidget.setRowCount(
-                self.tableWidget.rowCount() + 1)
-            for j, elem in enumerate(row):
-                self.tableWidget.setItem(
-                    i, j, QTableWidgetItem(str(elem)))
+        result = cur.execute(query).fetchall()
+        # Заполнили размеры таблицы
+        if not result:
+            self.label.setText('Ничего не нашлось')
+            return
+        else:
+            self.label.setText('')
+        self.tableWidget.setRowCount(len(result))
+        self.tableWidget.setColumnCount(len(result[0]))
+        self.titles = [description[0] for description in cur.description]
+        # Заполнили таблицу полученными элементами
+        for i, elem in enumerate(result):
+            for j, val in enumerate(elem):
+                self.tableWidget.setItem(i, j, QTableWidgetItem(str(val)))
+        self.modified = {}
+
+    def item_changed(self, item):
+        # Если значение в ячейке было изменено,
+        # то в словарь записывается пара: название поля, новое значение
+        if self.tableWidget.item(0, item.row()):
+            a = self.tableWidget.item(0, item.row()).text()
+            self.modified[self.titles[item.column()]] = item.text(), a
+
+    def save_results(self):
+        if self.modified:
+            cur = self.con.cursor()
+            que = "UPDATE coffee SET\n"
+            for key in self.modified.keys():
+                a = "{}='{}'\n".format(key, self.modified[key][0])
+                cur.execute(f'{que} {a} WHERE ID = {self.modified[key][1]}')
+            self.con.commit()
+            self.modified.clear()
 
     def closeEvent(self, event):
-        self.connection.close()
+        self.con.close()
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = DBSample()
+    ex = MyWidget()
     ex.show()
     sys.exit(app.exec())
