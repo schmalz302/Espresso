93e22e089e8a15e711493331fc8052c951bab9c3
buk55 <boltneffe@yandex.ru>
2021-01-13 15:38:05 +0500

    переделал таблицу

--- main.py
+++ main.py
@@ -12,18 +12,16 @@ class DBSample(QMainWindow):
         uic.loadUi('main.ui', self)
         self.connection = sqlite3.connect("coffee.sqlite.db")
         self.pushButton.clicked.connect(self.select_data)
-        # По умолчанию будем выводить все данные из таблицы films
         self.textEdit.setPlainText("SELECT * FROM coffee")
         self.select_data()
 
     def select_data(self):
-        # Получим результат запроса, 
-        # который ввели в текстовое поле
         query = self.textEdit.toPlainText()
         res = self.connection.cursor().execute(query).fetchall()
         # Заполним размеры таблицы
-        self.tableWidget.setColumnCount(4)
-        self.tableWidget.setRowCount(0)
+        self.tableWidget.setColumnCount(len(res[0]))
+        self.tableWidget.setRowCount(len(res))
+        print(len(res))
         # Заполняем таблицу элементами
         for i, row in enumerate(res):
             self.tableWidget.setRowCount(
@@ -33,8 +31,6 @@ class DBSample(QMainWindow):
                     i, j, QTableWidgetItem(str(elem)))
 
     def closeEvent(self, event):
-        # При закрытии формы закроем и наше соединение 
-        # с базой данных
         self.connection.close()
 
 
